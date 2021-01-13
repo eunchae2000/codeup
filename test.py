be8d5ac6db868f3152ad5a04fb148f2ac9fe60a4
eunchae2000 <tldjs3651@naver.com>
2021-01-13 21:23:55 +0900

    codeup 문제 1041-1050번

--- test.py
+++ test.py
@@ -1,49 +1,93 @@
-# 1031 10진수 정수 1개 입력받아 8진수로 출력
+# codeup 문제 1041~1050 풀이
 
-# a = int(input())
-# print(oct(a))
+#codeup 1041 문자 1개를 입력받아 다음 문자 출력
+"""
+a = input()
+a_1 = ord(a)
 
-# 1032 10진 정수 입력받아 16진수로 출력하기1 => 소문자
+print(chr(a_1+1))
+"""
 
-# a  = int(input())
-# print(hex(a))
+# 1042 정수 2개 입력받아 나눈 몫 값 출력
+"""
+a, b = input().split(" ")
 
-# 1033 10진 정수 입력받아 16진수로 출력하기2 => 대문자
+x = int(a)
+y = int(b)
 
-# b = int(input())
-# print(str(hex(b)).upper())
+print("%d" %(x/y))
+"""
 
-# 1034 8진 정수 1개 입력받아 10진수로 출력
+# 1043 정수 2개 입력받아 나눈 나머지 값 출력
+"""
+a,b = input().split(" ")
+x = int(a)
+y = int(b)
 
-# a = input()
-# print(int(a,8))
+print("%d" %(x%y))
+"""
+# 1044 정수 1개 입력받아 1을 더한 값 출력
+"""
+a = input()
+b = int(a)
 
-# 1035 16진 정수 1개 입력받아 8진수로 출력
+print("%d" %(b+1))
+"""
+# 1045 정수 2개를 입력받아 합, 차, 곱, 나누기 몫, 나누기 나머지, 나누기 소수점 둘째자리 출력
+"""
+a,b = input().split(" ")
+x = int(a)
+y = int(b)
 
-# a = input()
-# print(oct(int(a,16)))
+print("%d" %(x+y))
+print("%d" %(x-y))
+print("%d" %(x*y))
+print("%d" %(x/y))
+print("%d" %(x%y))
+print("%.2f" %(x/y))
+"""
+# 1046 정수 3개를 입력받아 합과 평균 출력
+"""
+a,b,c = input().split(" ")
+x = int(a)
+y = int(b)
+z = int(c)
 
-# 1036 영문자 1개 입력받아 10진수로 출력
+print("%d" %(x+y+z))
+print("%.1f" %((x+y+z)/3))
+"""
+# 1047 정수 1개 입력받아 2배의 값 출력
+"""
+a = input()
+b = int(a)
 
-# a = input()
-# print(ord(a))
+print("%d" %(b*2))
+  """
+# 1048 정수 2개 입력받아 2의 거듭제곱 값 출력
+"""
+a = input()
+b = input()
 
-# 1037 정수 입력받아 아스키 문자로 출력
+x = int(a)
+y = int(b)
 
-# a = int(input())
-# print(chr(a))
+print("%d" %(x<<y))
+"""
+# 1049 정수 2개 입력받아 크기 비교 => a가 b보다 크면 1을 출력
+"""
+a,b = input().split(" ")
+x = int(a)
+y = int(b)
+z = int(a>b)
 
-# 1038 정수 2개 입력받아 합 출력하기1 => int형 범위
+print(z)
+"""
+# 1050 정수 2개 입력받아 같으면 1 출력, 같지 않으면 0 출력
+"""
+a, b = input().split(" ")
+x = int(a)
+y = int(b)
+z = int(a==b)
 
-# a,b = input().split(" ")
-# print("두 수의 합은 %d" % (int(a)+int(b)))
-
-# 1039 정수 2개 입력박아 합 출력하기2 => long형 범위 (1보다 범위가 더 큼)
-
- # a,b = input().split(" ")
- # print("두 수의 합은 %d" % (int(a)+int(b)))
-
-# 1040 정수 1개 입력받아 부호 바꿔 출력
-
-# a = int(input())
-# print("%d" %-a)
+print(z)
+"""
