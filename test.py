96bcb0d9fac06f669251707bdb1e1a5c78097d9f
eunchae2000 <tldjs3651@naver.com>
2021-01-14 16:37:17 +0900

    codeup 문제 1051-1060

--- test.py
+++ test.py
@@ -1,93 +1,84 @@
-# codeup 문제 1041~1050 풀이
+# 1051 두 정수를 입력받아 b가 a보다 크거나 같으면 1을, 그렇지 않으면 0을 출력
 
-#codeup 1041 문자 1개를 입력받아 다음 문자 출력
-"""
-a = input()
-a_1 = ord(a)
-
-print(chr(a_1+1))
-"""
-
-# 1042 정수 2개 입력받아 나눈 몫 값 출력
 """
 a, b = input().split(" ")
+c = int(b>a)
 
-x = int(a)
-y = int(b)
-
-print("%d" %(x/y))
+print(c)
 """
 
-# 1043 정수 2개 입력받아 나눈 나머지 값 출력
-"""
-a,b = input().split(" ")
-x = int(a)
-y = int(b)
+# 1052 두 정수를 입력받아 a와 b가 서로 다르면 1을, 그렇지 않으면 0을 출력
 
-print("%d" %(x%y))
 """
-# 1044 정수 1개 입력받아 1을 더한 값 출력
-"""
-a = input()
-b = int(a)
+a, b = input().split()
+c = int(a!=b)
 
-print("%d" %(b+1))
+print(c)
 """
-# 1045 정수 2개를 입력받아 합, 차, 곱, 나누기 몫, 나누기 나머지, 나누기 소수점 둘째자리 출력
-"""
-a,b = input().split(" ")
-x = int(a)
-y = int(b)
 
-print("%d" %(x+y))
-print("%d" %(x-y))
-print("%d" %(x*y))
-print("%d" %(x/y))
-print("%d" %(x%y))
-print("%.2f" %(x/y))
-"""
-# 1046 정수 3개를 입력받아 합과 평균 출력
-"""
-a,b,c = input().split(" ")
-x = int(a)
-y = int(b)
-z = int(c)
+# 1053 1(참) 또는 0(거짓)이 입력되었을 때 반대로 출력
 
-print("%d" %(x+y+z))
-print("%.1f" %((x+y+z)/3))
 """
-# 1047 정수 1개 입력받아 2배의 값 출력
+a = int(input())
+print(int(not a))
 """
-a = input()
-b = int(a)
 
-print("%d" %(b*2))
-  """
-# 1048 정수 2개 입력받아 2의 거듭제곱 값 출력
-"""
-a = input()
-b = input()
+# 1054 두 개의 참(1) 또는 거짓(0)이 입력될 때, 모두 참일 때에만 참을 출력
 
-x = int(a)
-y = int(b)
-
-print("%d" %(x<<y))
-"""
-# 1049 정수 2개 입력받아 크기 비교 => a가 b보다 크면 1을 출력
-"""
-a,b = input().split(" ")
-x = int(a)
-y = int(b)
-z = int(a>b)
-
-print(z)
-"""
-# 1050 정수 2개 입력받아 같으면 1 출력, 같지 않으면 0 출력
 """
 a, b = input().split(" ")
-x = int(a)
-y = int(b)
-z = int(a==b)
+c = int((a==b))
+print(c)
+"""
 
-print(z)
+# 1055 두 개의 참(1) 또는 거짓(0)이 입력될 때, 하나라도 참이면 참을 출력
+
+"""
+a, b = map(int,input().split())
+if(bool(a) or bool(b)):
+  print("1")
+else:
+  print("0")
+"""
+
+# 1056두 가지의 참(1) 또는 거짓(0)이 입력될 때, 참/거짓이 서로 다를 때에만 참을 출력
+
+"""
+a, b = input().split(" ")
+c = int((a!=b))
+
+print(c)
+"""
+
+# 1057 두 개의 참(1) 또는 거짓(0)이 입력될 때, 참/거짓이 서로 같을 때에만 참을 출력
+
+"""
+a, b = input().split(" ")
+c = int(a==b)
+
+print(c)
+"""
+
+# 1058 두 개의 참(1) 또는 거짓(0)이 입력될 때, 모두 거짓일 때에만 참이 출력
+
+"""
+a, b = map(int,input().split(" "))
+if((a==0)and(b==0)):
+  print("1")
+else:
+  print("0")
+"""
+
+# 1059 입력된 정수를 비트단위로 참/거짓을 바꾼 후 정수로 출력
+
+"""
+a = int(input())
+print(~a)
+"""
+
+# 1060 입력된 정수 두 개를 비트단위로 and 연산한 후 그 결과를 정수로 출력
+
+"""
+a, b = map(int, input().split(" "))
+print(a&b)
 """
