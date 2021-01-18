2c352ae73e6b8c33c96fbfe4d485a7ed67305ed4
eunchae2000 <tldjs3651@naver.com>
2021-01-18 20:30:09 +0900

    codeup 문제 1091-1100 (1097, 1099 제외)

--- test.py
+++ test.py
@@ -1,114 +1,109 @@
-# 1081 주사위를 2개 던지면?
-
+# 1091 수 나열하기 3  => 시작값(a), 곱할 값(b), 더할 값(c), 몇 번째 인지를 나타내는 정수(d)
 """
-a, b = map(int, input().split())
+a, b, c, d = map(int, input().split(" "))
 
-for i in range(1, a+1):
-  for j in range(1, b+1):
-    print(i, j)
+for i in range(d-1):
+  a = a * b + c
+print(a)
 """
-
-# 1082 16진수 구구단?
-
-"""
-print("16진수를 입력하세요.")
-a = int(input(), 16)
-b = hex(a)[2].upper()
-
-for i in range(1, 16):
-  c = hex(a*i)[2:].upper()
-  d = hex(i)[2:].upper()
-  print(b+" * "+d+" = "+c)
-"""
-
-# 1083 3 6 9 게임의 왕이 되자
-
-"""
-a = int(input())
-
-for i in range(1, a+1):
-  if(i % 3 == 0):
-    print("X", end = " ")
-  else:
-    print(i, end = " ")
-"""
-
-# 1084 빛 섞어 색 만들기
-
+# 1092 함께 문제 푸는 날 => 최소공배수
+# 3명의 사람이 3, 7, 9일 마다 한 번씩 들어온다면, 처음 가입하고 63일 만에 다시 3명이 함께 문제를 풀게 되는것
 """
 a, b, c = map(int, input().split(" "))
-count = 0
+d = 1
 
+while(d % a != 0 or d % b != 0 or d % c != 0):
+  d += 1
+print(d)
+"""
+# 1093 이상한 출석 번호 부르기 1 ★
+"""
+a = int(input())
+b = map(int, input().split())
+blist = list(b)
+arr = []
+
+for i in range(24):
+  arr.append(0)
+for i in range(a):
+  arr[blist[i]] += 1
+for i in range(1, 24):
+  print(arr[i], end = " ")
+"""
+# 1094 이상한 출석 번호 부르기 2 ★
+"""
+a = int(input())
+b = map(int, input().split())
+arr = []
+
+for i in b:
+  arr.append(i)
+
+arr.reserve()
+
+for j in range(a):
+  print(arr[j], end = ")
+"""
+# 1095 이상한 출석 번호 부르기 3 ★
+"""
+a = int(input())
+b = map(int, input().split())
+arr = []
+min_arr = []
+
+for i in b:
+  arr.append(i)
+
+for j in range(a):
+  min_arr.append(arr[j])
+
+print(min(min_arr))
+"""
+# 1096 바둑판에 흰 돌 놓기
+"""
+a = []
+for i in range(20):
+  a.append([])
+  for j in range(20):
+    a[i].append(0)
+
+n = int(input())
+for i in range(n):
+  x,y = input().split()
+  a[int(x)][int(y)] = 1
+
+for i in range(1, 20):
+  for j in range(1, 20):
+    print(a[i][j], end = " ")
+  print()
+"""
+# 1097 바둑알 십자 뒤집기
+
+# 1098 설탕과자 뽑기
+"""
+h, w = map(int, input().split())
+arr = []
+for i in range(h):
+  arr.append([])
+  for j in range(w):
+    arr[i] = 0
+
+n = int(input())
+for i in range(n):
+  l, d, x, y = map(int, input().split())
+  for j in range(l):
+    if(d==0):
+      arr[x][y + j] = 1
+    else:
+      arr[x + j][y] = 1
 for i in range(a):
   for j in range(b):
-    for k in range(c):
-      print(i, j, k)
-      count += 1
-print(count)
+    print(arr[i][j], end = "")
+  print()
 """
+# 1099 성실한 개미
 
-# 1085 소리 파일 저장용량 계산하기
-
+# 1100 Hello, World!
 """
-a, b, c, d = map(int, input().split())
-aa = (a*b*c*d)/8/1024/1024    => MB 단위로 바꿔주기 위해서 1024 계산
-
-print(round(aa, 1), 'MB')   => round 함수는 반올림 함수로 소수점 첫째자리까지 반올림 한다는 것
-"""
-
-# 1086 그림 파일 저장용량 계산하기
-
-"""
-a, b, c = map(int, input().split())
-x = (a*b*c)/8/1024/1024
-
-print(round(x, 2), 'MB')
-"""
-
-# 1087 여기까지! 이제 그만~
-
-"""
-a = int(input())
-b = 0
-for i in range(1, a):
-  b += i
-  if b>=a:
-    break
-  else:
-    i += i
-print(b)
-"""
-
-# 1088 3의 배수는 통과?
-
-"""
-a = int(input())
-
-for i in range(a+1):
-  if i % 3 == 0:
-    continue
-  else:
-    print(i, end=' ')
-"""
-
-# 1089 수 나열하기1
-
-"""
-a, b, c = map(int, input().split())
-
-for i in range(c+1):
-  j = a + (i - 1) * b
-print(j)
-"""
-
-# 1090 수 나열하기2
-
-"""
-a, b, c = map(int, input().split())
-for i in range(c+1):
-  if b != 0:
-    y = a*b**(i-1)
-  else:
-    y = 0
-print(y)
+print("Hello, World!")
 """
