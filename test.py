55b209d008ee1ffb78dae3c387f67bec4e91a7fa
eunchae2000 <tldjs3651@naver.com>
2021-01-21 13:18:03 +0900

    codeup 문제 1121-1130

--- test.py
+++ test.py
@@ -1,83 +1,86 @@
-# 1111 % 출력
+# 1121 나머지 구하기
+
+"""
+a, b = map(int, input().split())
+c = a%b
+
+print(c)
+"""
+
+# 1122 초를 분/초로 변환
+
+"""
+second = int(input())
+a = second/60
+b = second%60
+
+print("%.0f %d" %(a, b))
+"""
+
+# 1123 섭씨 온도를 화씨 온도로 변환
+
+"""
+c = int(input())
+
+h = (9/5)*c+32
+print("%.3f" %h)
+"""
+
+# 1124 분자량 구하기1
+
+"""
+a = input()
+key = a.index('H')
+num1 = a[1:key]
+num2 = a[key+1:]
+print(int(num1)*12 + int(num2)*1)
+"""
+
+# 1125 8진수 16진수 변환
 
 """
 a = int(input())
-
-print(a,'%')
+print("%o %X" %(a, a))
 """
 
-# 1112 두 정수 출력
-
-"""
-a, b = map(int, input().split())
-print(a, b)
-"""
-
-# 1113 바꿔서 출력하기
-
-"""
-a, b = map(int, input().split())
-print(b, a)
-"""
-
-# 1114 두 정수의 덧셈
+# 1126 정수 계산기
 
 """
 a, b = map(int, input().split())
 c = a+b
-print(c)
+d = a-b
+e = a*b
+f = a/b
+g = a%b
+
+print("%d + %d = %d" %(a, b, c))
+print("%d - %d = %d" %(a, b, d))
+print("%d X %d = %d" %(a, b, e))
+print("%d / %d = %d" %(a, b, f))
+print("%d '%' %d = %d" %(a, b, g))
+
+or
+
+num1, num2 = input().split()
+op = ['+', '-', '*', '/', '%']
+for i in range(5): print(num1 + ' ' + op[i] + ' ' + num2 + ' =', int(eval(num1+op[i]+num2)))
 """
 
-# 1115 두 정수의 덧셈(int 범위 보다 큼)
+# 1127 성적 계산
 
 """
-a, b = map(int, input().split())
-c = a+b
-print(c)
+sum = 0
+for i in range(3):
+    a, b = input().split()
+    sum = sum + (float(a) * int(b))
+print(sum)
 """
 
-# 1116 사칙연산 계산기
+# 1128 n*123456789
 
 """
-a, b = map(int, input().split())
-
-print("%d + %d = %d" %(a, b, a+b))
-print("%d - %d = %d" %(a, b, a-b))
-print("%d X %d = %d" %(a, b, a*b))
-print("%d / %d = %d" %(a, b, a/b))
-"""
-
-# 1117 두 실수의 곱
-
-"""
-a, b = map(float, input().split())
-
-print("%.2f" % (a*b))
-"""
-
-# 1118 삼각형의 넓이 구하기
-
-"""
-a, b = map(int, input().split())
-c = (a*b)/2
-
-print("%.1f" %c)
-"""
-
-# 1119 일을 시간으로 변환
-
-"""
-a  = int(input())
-b = a*24
+a = int(input())
+b = a*123456789
 
 print(b)
-"""
-
-# 1120 세 수의 평균
-
-"""
-a, b, c = map(int, input().split())
-x = (a+b+c)/3
-
-print("%.2f" %x)
 """
