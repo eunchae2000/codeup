5bcd67c8297ee68201370288ba6832b705706153
eunchae2000 <tldjs3651@naver.com>
2021-01-22 19:05:42 +0900

    codeup 문제 1131-1140

--- test.py
+++ test.py
@@ -1,86 +1,94 @@
-# 1121 나머지 구하기
+# 1131 문자 출력하기
+
+"""
+a = str(input())
+
+print(a)
+"""
+
+# 1132 문자열 출력하기
+
+"""
+a = str(input())
+
+print(a)
+"""
+
+# 1133 공백이 있는 문자열 입출력
+
+"""
+a, b, c = input().split()
+
+print(a, b, c)
+"""
+
+# 1135 관계 연산자 1
 
 """
 a, b = map(int, input().split())
-c = a%b
-
-print(c)
+if a>b:
+    print("1")
+else:
+    print("0")
 """
 
-# 1122 초를 분/초로 변환
-
-"""
-second = int(input())
-a = second/60
-b = second%60
-
-print("%.0f %d" %(a, b))
-"""
-
-# 1123 섭씨 온도를 화씨 온도로 변환
-
-"""
-c = int(input())
-
-h = (9/5)*c+32
-print("%.3f" %h)
-"""
-
-# 1124 분자량 구하기1
-
-"""
-a = input()
-key = a.index('H')
-num1 = a[1:key]
-num2 = a[key+1:]
-print(int(num1)*12 + int(num2)*1)
-"""
-
-# 1125 8진수 16진수 변환
-
-"""
-a = int(input())
-print("%o %X" %(a, a))
-"""
-
-# 1126 정수 계산기
+# 1136 관계 연산자 2
 
 """
 a, b = map(int, input().split())
-c = a+b
-d = a-b
-e = a*b
-f = a/b
-g = a%b
-
-print("%d + %d = %d" %(a, b, c))
-print("%d - %d = %d" %(a, b, d))
-print("%d X %d = %d" %(a, b, e))
-print("%d / %d = %d" %(a, b, f))
-print("%d '%' %d = %d" %(a, b, g))
-
-or
-
-num1, num2 = input().split()
-op = ['+', '-', '*', '/', '%']
-for i in range(5): print(num1 + ' ' + op[i] + ' ' + num2 + ' =', int(eval(num1+op[i]+num2)))
+if (a==b):
+    print("1")
+else:
+    print("0")
 """
 
-# 1127 성적 계산
+# 1137 관계 연산자 3
 
 """
-sum = 0
-for i in range(3):
-    a, b = input().split()
-    sum = sum + (float(a) * int(b))
-print(sum)
+a, b = map(int, input().split())
+if (a!=b):
+    print("1")
+else:
+    print("0")
 """
 
-# 1128 n*123456789
+# 1138 논리 연산자(NOT)
 
 """
 a = int(input())
-b = a*123456789
+if (a==1):
+    print("0")
+elif (a==0):
+    print("1")
+else:
+    print("0 또는 1만 입력하세요.")
+"""
 
-print(b)
+# 1139 논리 연산자(AND)
+
+"""
+a, b = map(int, input().split())
+if ((a == 1) and (b == 1)):
+    print("1")
+elif((a == 0) and (b == 1)):
+    print("0")
+elif((a == 1) and (b == 0)):
+    print("0")
+elif((a == 0) and (b == 0)):
+    print("0")
+else:
+    print("입력을 확인해 주세요.")
+"""
+
+# 1140 논리 연산자(OR)
+
+"""
+a, b= map(int, input().split())
+
+if ((a==1) or (b==1)):
+    print("1")
+elif ((a==0) and (b==0)):
+    print("0")
+else:
+    print("숫자를 확인해 주세요.")
 """
