38cc24c04ddb346518c6d2685458e5e1b2612fbe
eunchae2000 <tldjs3651@naver.com>
2021-01-26 16:43:59 +0900

    codeup 문제 1171-1180

--- test.py
+++ test.py
@@ -1,144 +1,76 @@
-# 1161 홀수와 짝수 그리고 더하기
-
+# 1171 당신의 학번은? 2
 """
-a, b = map(int, input().split())
-if a%2==0:
-    if b%2==0:
-        print("짝수 + 짝수 = 짝수")
+grade, room, num = map(int, input().split())
+if room<10:
+    room = '0' + str(room)
+    if num<10:
+        num = '00' + (str)(num)
+    elif num<100:
+        num = '0' + (str)(num)
     else:
-        print("짝수 + 홀수 = 홀수")
+        num = num
+
+a = str(grade) + room + num
+print(a)
+"""
+
+# 1172 세 수 정렬하기
+"""
+number = list(map(int, input().split()))
+number.sort()                   // 수를 작은 것부터 나열하기 위해서 파이썬에서는 sort라는 함수를 사용
+
+print(number[0], number[1], number[2])
+"""
+
+# 1173 30분전
+"""
+time, minute = map(int, input().split())
+
+if (minute-30<0):
+    time = time - 1
+    minute = minute +30
+    print(time, minute)
+elif (minute-30>0):
+    time = time
+    minute = minute - 30
+    print(time, minute)
+elif (minute - 30 == 0):
+    time = time
+    minute = minute - 30
+    print(time, 0+minute)
 else:
-    if b%2==0:
-        print("홀수 + 짝수 = 홀수")
-    else:
-        print("홀수 + 홀수 = 짝수")
+    print("시간을 확인해 주세요.")
 """
 
-# 1162 당신의 사주를 봐 드립니다 1 => 년-월+일 => 마지막의 숫자가 0이면 "대박"을 출력, 그렇지 않으면 "그럭저럭"을 출력
-
+# 1174 30분전 (if는 아직)
 """
-a, b, c = map(int, input().split(" "))
-if ((a-b+c)%10==0):
-    print("대박")
+hour, minute = map(int, input().split())
+
+hour += 24
+minute = minute + hour * 60
+minute -= 30
+hour = minute / 60
+hour = hour % 24
+minute = minute % 60
+
+print(int(hour), int(minute))
+"""
+
+
+# 1180 만능 휴지통
+"""
+a = input()  => String 형태로 받음
+
+b = a[1] + a[0]  => String 형태로 입력받기 때문에 별 다른 함수 없이 자리 바꾸기 가능
+b = int(b) * 2
+
+if(b > 99):
+    b = b % 100
+
+if(b <= 50):
+    print(b)
+    print('GOOD')
 else:
-    print("그럭저럭")
-"""
-
-# 1163 당신의 사주를 봐 드립니다 2
-
-"""
-y, m, d = map(int, input().split())
-n = int(y+m+d) % 1000
-n = int(n / 100)
-
-if n%2==0:
-    print("대박")
-else:
-    print("그럭저럭")
-"""
-
-# 1164 터널 통과하기 1
-
-"""
-a, b, c = map(int, input().split())
-if (a<=170 and b<=170 and c<=170):
-    print("PASS")
-else:
-    print("CRASH")
-"""
-
-# 1165 축구의 신 1
-
-"""
-a, b = map(int, input().split())
-c = 90-a
-d = c/5
-e = c%5
-print("%.0f"%(b+d+e))
-"""
-
-# 1166 윤년 판별
-
-"""
-a = int(input())
-if ((a%4==0) and (a%100!=0)):
-    print("YES")
-elif (a%400==0):
-    print("YES")
-else:
-    print("NO")
-"""
-
-# 1167 두 번째로 작은 수
-
-"""
-a, b, c = map(int, input().split())
-if a>b:
-    if b>c:
-        print(b)
-    elif a<c:
-        print(a)
-    else:
-        print(c)
-elif b>c:
-    if a>b:
-        print(b)
-    elif c>a:
-        print(a)
-    else:
-        print(c)
-elif a>c:
-    if a<b:
-        print(a)
-    elif b>c:
-        if a>b:
-            print(b)
-        else:
-            print(a)
-    else:
-        print(c)
-"""
-
-# 1168 나이 계산 1
-
-"""
-birth, gender = input().split()
-gender = int(gender)
-
-if (gender == 1):
-    birthday = int('19' + birth[0:2])
-    print(2021 - birthday + 1)
-elif (gender == 2):
-    birthday = int('19' + birth[0:2])
-    print(2021 - birthday + 1)
-elif (gender == 3):
-    birthday = int('20' + birth[0:2])
-    print(2021 - birthday + 1)
-elif (gender == 4):
-    birthday = int('20' + birth[0:2])
-    print(2021 - birthday + 1)
-else:
-    print("숫자를 확인해 주세요")
-"""
-
-# 1169 나이 계산 2
-
-"""
-a = int(input())
-if (a>22):
-    b = str(2021 - a)
-    print(int(b[2:4]), 1)
-else:
-    b = str(2021 - a)
-    print(int(b[2:4]), 3)
-"""
-
-# 1170 당신의 학번은? 1
-
-"""
-a, b, c = map(int, input().split())
-if c<10:
-    print(str(a,b,"0",c))
-else:
-    print(str(a,b,c))
+    print(b)
+    print("Oh My God")
 """
