905daf94e3fd0d1e685161664010448c53e8fb0d
eunchae2000 <tldjs3651@naver.com>
2021-01-27 15:27:34 +0900

    codeup 문제 1201-1210

--- test.py
+++ test.py
@@ -1,76 +1,129 @@
-# 1171 당신의 학번은? 2
+# 1201 정수 판별
 """
-grade, room, num = map(int, input().split())
-if room<10:
-    room = '0' + str(room)
-    if num<10:
-        num = '00' + (str)(num)
-    elif num<100:
-        num = '0' + (str)(num)
-    else:
-        num = num
-
-a = str(grade) + room + num
-print(a)
-"""
-
-# 1172 세 수 정렬하기
-"""
-number = list(map(int, input().split()))
-number.sort()                   // 수를 작은 것부터 나열하기 위해서 파이썬에서는 sort라는 함수를 사용
-
-print(number[0], number[1], number[2])
-"""
-
-# 1173 30분전
-"""
-time, minute = map(int, input().split())
-
-if (minute-30<0):
-    time = time - 1
-    minute = minute +30
-    print(time, minute)
-elif (minute-30>0):
-    time = time
-    minute = minute - 30
-    print(time, minute)
-elif (minute - 30 == 0):
-    time = time
-    minute = minute - 30
-    print(time, 0+minute)
+num = int(input())
+if num>0:
+    print("양수")
+elif num == 0:
+    print("0")
 else:
-    print("시간을 확인해 주세요.")
+    print("음수")
 """
 
-# 1174 30분전 (if는 아직)
+# 1202 등급 판정
 """
-hour, minute = map(int, input().split())
-
-hour += 24
-minute = minute + hour * 60
-minute -= 30
-hour = minute / 60
-hour = hour % 24
-minute = minute % 60
-
-print(int(hour), int(minute))
-"""
-
-
-# 1180 만능 휴지통
-"""
-a = input()  => String 형태로 받음
-
-b = a[1] + a[0]  => String 형태로 입력받기 때문에 별 다른 함수 없이 자리 바꾸기 가능
-b = int(b) * 2
-
-if(b > 99):
-    b = b % 100
-
-if(b <= 50):
-    print(b)
-    print('GOOD')
+result = int(input())
+if result >= 90:
+    print("A")
+elif result >= 80:
+    print("B")
+elif result >= 70:
+    print("C")
+elif result >= 60:
+    print("D")
+elif result < 60:
+    print("F")
 else:
-    print(b)
-    print("Oh My God")
+    print("수를 확인해 주세요")
+"""
+
+# 1203 비만도 측정 0
+"""
+print("BMI 수치를 입력해 주세요.")
+weight = int(input())
+
+if weight>0 and weight<=10:
+    print("정상 입니다.")
+elif weight >10 and weight <= 20:
+    print("과체중 입니다.")
+elif weight > 20:
+    print("비만 입니다.")
+else:
+    print("BMI 수치를 확인해 주시기 바랍니다.")
+"""
+
+# 1204 영어 서수로 표현하기
+"""
+num = int(input())
+result = ""
+
+if num % 10 == 1:
+    if num == 11:
+        result = (str(num) + "th")
+    result = (str(num) + "st")
+
+elif num % 10 == 2:
+    if num == 12:
+        result = (str(num) +"th")
+    result = (str(num) +"nd")
+
+elif num % 10 == 3:
+    if num == 13:
+        result = (str(num) + "th")
+    result = (str(num) + "rd")
+
+else:
+    result = (num + "th")
+
+print(result)
+"""
+
+# 1205 최댓값
+"""
+data, op = [], ['+', '-', '*', '/', '**']
+
+num1, num2 = input().split()
+
+data = [eval(num1+op[i]+num2) for i in range(len(op))]
+
+for i in range(len(op)):
+    data.append(eval(num2+op[i]+num1))
+
+print('%.6f' % (max(data)))
+"""
+
+# 1206 배수
+"""
+num1, num2 = map(int, input().split())
+if num1 % num2 == 0:
+    print("%d x X = %d" %(num2, num1))
+elif num2 % num1 == 0:
+    print("%d x X = %d" %(num1, num2))
+else:
+    print("none")
+"""
+
+# 1207 윷놀이 => 윷이 뒤집어 졌으면 1 안뒤어 졌으면 0을 입력(4개)
+"""
+yut = list(map(int, input().split()))
+a = 0
+
+for i in range(0, 4):
+    if yut[i] == 1:
+        a += 1
+
+if a == 1:
+    print("도")
+elif a == 2:
+    print("개")
+elif a == 3:
+    print("걸")
+elif a == 4:
+    print("윷")
+elif a == 0:
+    print("모")
+else:
+    print("윷을 다시 확인해 주세요")
+"""
+
+# 1210 칼로리 계산하기
+"""
+menu = [0, 400, 340, 170, 100, 70]
+menu1, menu2 = map(int, input().split())
+
+menu_result = menu[menu1] + menu[menu2]
+
+if (menu_result < 500):
+    print("No angry")
+else:
+    print("Angry")
 """
