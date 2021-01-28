2b55903d24af63276a180ba255cd999c901fc533
eunchae2000 <tldjs3651@naver.com>
2021-01-28 11:51:23 +0900

    codeup 문제 1212-1222

--- test.py
+++ test.py
@@ -1,129 +1,79 @@
-# 1201 정수 판별
+# 1212 삼각형의 성립 조건
+
 """
-num = int(input())
-if num>0:
-    print("양수")
-elif num == 0:
-    print("0")
+a, b, c = map(int, input().split())
+if (a<b+c):
+    print("yes")
+elif (b<a+c):
+    print("yes")
+elif (c<a+b):
+    print("yes")
 else:
-    print("음수")
+    print("No")
 """
 
-# 1202 등급 판정
+# 1214 이 달은 며칠까지 있을까?
+
 """
-result = int(input())
-if result >= 90:
-    print("A")
-elif result >= 80:
-    print("B")
-elif result >= 70:
-    print("C")
-elif result >= 60:
-    print("D")
-elif result < 60:
-    print("F")
+year, month = map(int, input().split())
+if (month==1 or month==3 or month== 5 or month==7 or month==8 or month==10 or month==12):
+    print("31")
+elif (month==4 or month==6 or month==9 or month==11):
+    print("30")
+elif (month==2):
+    if(year%400==0):
+        if(year%4==0 and year%100!=0):
+            print("29")
+    else:
+        print("28")
 else:
-    print("수를 확인해 주세요")
+    print("연도와 월을 확인해 주세요")
 """
 
-# 1203 비만도 측정 0
-"""
-print("BMI 수치를 입력해 주세요.")
-weight = int(input())
+# 1216 컨설팅 회사
 
-if weight>0 and weight<=10:
-    print("정상 입니다.")
-elif weight >10 and weight <= 20:
-    print("과체중 입니다.")
-elif weight > 20:
-    print("비만 입니다.")
+"""
+a, b, c = map(int, input().split())
+if (a > b-c):
+    print("do not advertise")
+elif (a < b-c):
+    print("advertise")
+elif (a == b-c):
+    print("does not matter")
 else:
-    print("BMI 수치를 확인해 주시기 바랍니다.")
+    print("이익을 다시 확인하세요")
 """
 
-# 1204 영어 서수로 표현하기
+# 1218 삼각형 판단하기
+
 """
-num = int(input())
-result = ""
+print("삼각형의 3변의 길이를 입력하세요 단, a <= b <= c")
+a, b, c = map(int, input().split())
 
-if num % 10 == 1:
-    if num == 11:
-        result = (str(num) + "th")
-    result = (str(num) + "st")
-
-elif num % 10 == 2:
-    if num == 12:
-        result = (str(num) +"th")
-    result = (str(num) +"nd")
-
-elif num % 10 == 3:
-    if num == 13:
-        result = (str(num) + "th")
-    result = (str(num) + "rd")
-
+if (a==b==c):
+    print("정삼각형")
+elif (a==b or a==c or b==c):
+    print("이등변삼각형")
+elif (a*a + b*b == c*c):
+    print("직각삼각형")
+elif (a<= b<= c):
+    print("삼각형")
 else:
-    result = (num + "th")
-
-print(result)
+    print("삼각형이 아님")
 """
 
-# 1205 최댓값
+# 1222 축구의 신2
+
 """
-data, op = [], ['+', '-', '*', '/', '**']
-
-num1, num2 = input().split()
-
-data = [eval(num1+op[i]+num2) for i in range(len(op))]
-
-for i in range(len(op)):
-    data.append(eval(num2+op[i]+num1))
-
-print('%.6f' % (max(data)))
-"""
-
-# 1206 배수
-"""
-num1, num2 = map(int, input().split())
-if num1 % num2 == 0:
-    print("%d x X = %d" %(num2, num1))
-elif num2 % num1 == 0:
-    print("%d x X = %d" %(num1, num2))
+print("현재 경기 타임, 1반 득점, 2반 득점 순으로 입력해 주세요.")
+time, score1, score2 = map(int, input().split())
+score = (90-time)/5
+if (score1+score > score2):
+    print("win")
+elif (score1+score == score2):
+    print("same")
+elif (score+score1<score2):
+    print("lose")
 else:
-    print("none")
-"""
-
-# 1207 윷놀이 => 윷이 뒤집어 졌으면 1 안뒤어 졌으면 0을 입력(4개)
-"""
-yut = list(map(int, input().split()))
-a = 0
-
-for i in range(0, 4):
-    if yut[i] == 1:
-        a += 1
-
-if a == 1:
-    print("도")
-elif a == 2:
-    print("개")
-elif a == 3:
-    print("걸")
-elif a == 4:
-    print("윷")
-elif a == 0:
-    print("모")
-else:
-    print("윷을 다시 확인해 주세요")
-"""
-
-# 1210 칼로리 계산하기
-"""
-menu = [0, 400, 340, 170, 100, 70]
-menu1, menu2 = map(int, input().split())
-
-menu_result = menu[menu1] + menu[menu2]
-
-if (menu_result < 500):
-    print("No angry")
-else:
-    print("Angry")
+    print("득점을 다시 확인해 주세요")
 """
