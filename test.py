d8eb006ad73c1dba2ae449ac66ff383d20f899ba
eunchae2000 <tldjs3651@naver.com>
2021-01-31 22:07:53 +0900

    codeup 문제 1161-1170

--- test.py
+++ test.py
@@ -1,99 +1,75 @@
-# 1251 1부터 100까지 출력하기
-
+# 1161 First Special Judge
 """
-for i in range(1, 101):
-    print(i)
-"""
+a = list(map(int, input().split()))
+b = False
 
-# 1252 1부터 n까지 출력하기
-
-"""
-n = int(input())
-for i in range(1, n+1):
-    print(i)
-"""
-
-# 1253 a부터 b까지 출력하기
-
-"""
-a, b = map(int, input().split())
-
-if (a<b):
-    for i in range(a, b+1):
+for i in a:
+    if i % 5 == 0:
         print(i)
-else:
-    print("수를 확인해 주세요")
+        b = True
+        break
+
+if b == False:
+    print('0')
 """
 
-# 1254 알파벳 출력하기
-
+# 1165 구구단 출력하기1
 """
+num = int(input())
 
-a, b = map(ord, input().split())
-
-if a<b:
-    for i in range(a, b+1):
-        print(chr(i))
+for i in range(1, 10):
+    print(str(num)+" X "+str(i)+" = "+str(num*i))
 """
-
-# 1255 두 실수 사이 출력하기
+# 1166 n개의 수의 합
 """
-a, b = map(float, input().split())
-
-if (a<b):
-    for i in range(a, b, 0.01):
-        print("%.2f" %i)
-"""
-
-# 1256 별 출력하기
-
-"""
-n = int(input())
-for i in range(n):
-    print("*", end = "")
-"""
-
-# 1257 두 수 사이의 홀수 출력하기
-
-"""
-a, b = map(int, input().split())
-for i in range(a, b+1):
-    if (i%2!=0):
-        print(i, end = " ")
- """
-
-# 1258 1부터 n까지 합 구하기
-
-"""
-n = int(input())
-sum = 0
-
-for i in range(1, n+1):
-    sum += i
-    
-print(sum)
-"""
-
-# 1259 1부터 n가지 중 짝수의 합 구하기
-
-"""
-n = int(input())
+num = int(input())
+n = list(map(int, input().split()))
 sum1 = 0
-for i in range(1, n+1):
-    if i%2==0:
+
+for i in n:
+    sum1 += i
+
+print(sum1)
+"""
+# 1167 n개의 수 중 5의 배수의 합
+"""
+num = int(input())
+n = list(map(int, input().split()))
+sum1 = 0
+
+for i in n:
+    if i%5==0:
         sum1 += i
 
 print(sum1)
 """
-
-# 1260 3의 배수의 합
-
+# 1168 n개의 수 중 짝수의 개수 
 """
-a, b = map(int, input().split())
-summ = 0
-for i in range(a, b+1):
-    if (i%3==0):
-        summ += i
+num = int(input())
+n = list(map(int, input().split()))
+sum1 = 0
 
-print(summ)
+for i in n:
+    if i%2==0:
+        sum1 += 1
+print(sum1)
+"""
+# 1169 수열의 값 구하기
+"""
+a, b, c, n = map(int, input().split())
+sum1 = a
+
+for i in range(0, n-1):
+    sum1 = sum1 * b + c
+print(sum1)
+"""
+# 1170 1의 개수는? 1
+"""
+n = int(input())
+sum1 = 0
+
+for i in range(1, n+1):
+    if(i%10 == 1):
+        sum1 += 1
+print(sum1)
 """
