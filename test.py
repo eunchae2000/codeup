1f86d1c9e0f25c71fed5a7823da987c29d2105bd
coco <tldjs3651@naver.com>
2021-02-06 20:19:52 +0900

    codeup 문제 1361-1370

--- test.py
+++ test.py
@@ -1,187 +1,173 @@
-
-# 1294 시저의 암호2 ★
+# 1361 별 계단 만들기
 
 """
-text = input()
-password = ''
+num = int(input())
 
-for i in range(0, len(text)):
-    if text[i] == ' ':
-        password += text[i]
-    elif ord(text[i]) > 119:
-        if text[i] == 'x':
-            password += 'a'
-        elif text[i] == 'y':
-            password += 'b'
-        elif text[i] == 'z':
-            password += 'c'
-    else:
-        code = ord(text[i]) + 3
-        password += chr(code)
-        
-print(password)
+for i in range(0, num):
+    for j in range(0, i):
+        print(" ", end = " ")
+    print("**")
 """
 
-# 1297 단면의 최대 넓이 ★
+# 1362 숫자 피라미드 3
 
 """
 n = int(input())
-a = 0
-b = 0
+​
+​
+def get_max_num(n):
+    num = 0
+    for i in range(1, n+1):
+        num += i
+    return num
+​
+​
+number = get_max_num(n)
+for i in range(1, n+1):
+    for j in range(0, i):
+        print(number, end=' ')
+        number -= 1
+    print()
+"""
 
+# 1365 사각형 출력하기 3
+
+"""
+n = int(input())
+​
+for i in range(0, n):
+    print("*", end = " ")
+print()
+
+for i in range(1, n-1):
+    for j in range(0, n):
+        if(j==0 or j==1 or j==n-1 or j==n-i-1):
+            print("*", end = " ")
+        else:
+            print(" ", end = " ")
+    print()
+
+for i in range(0, n):
+    print("*", end = " ")
+print()
+"""
+
+# 1366 사각형 출력하기 4
+
+"""
+n = int(input())
+​
+for i in range(0, n):
+    print('*', end='')
+print()
+​
 for i in range(1, int(n/2)):
-    if a < i * (n-i * 2):
-        a = i * (n-i * 2)
-        b = i
-
-print(b)
-"""
-
-# 1351 구구단 출력하기2
-
-"""
-a, b = map(int, input().split())
-result = 1
-
-for i in range(a, b+1):
-    for j in range(1, 10):
-        result = i*j
-        print(str(i) + " X " + str(j) + " = " + str(result))
-    print(end = "")
-"""
-
-# 1352 사각형 출력하기 1
-
-"""
-n = int(input())
-
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        print("*", end = "")
-    print()
-"""
-
-# 1353 삼각형 출력하기 1
-
-"""
-n = int(input())
-
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        print("*", end = "")
-        if(j==i):
-            break
+    for j in range(0, n):
+        if (j == 0) or (j == i) or (j == int(n/2)) or (j == n-1) or (j == n-i-1):
+            print('*', end='')
         else:
-            continue
+            print(' ', end='')
     print()
-"""
-
-# 1354 삼각형 출력하기 2
-
-"""
-n = int(input())
-
-for i in range(n+1, 1, -1):
-    for j in range(1, i):
-        print("*", end = "")
-        if(j==i):
-            break
+​
+for i in range(0, n):
+    print('*', end='')
+print()
+​
+for i in range(int(n/2), 1, -1):
+    for j in range(0, n):
+        if (j == 0) or (j == i-1) or (j == int(n/2)) or (j == n-1) or (j == n-i):
+            print('*', end='')
         else:
-            continue
+            print(' ', end='')
     print()
+​
+for i in range(0, n):
+    print('*', end='')
+print()
 """
 
-# 1355 삼각형 출력하기 3
+# 1367 평행사변형 출력하기 1
 
 """
+응용버전 1
 n = int(input())
 
 for i in range(0, n):
-    for j in range(0, i):
-        print(" ", end = "")
-    for j in range(0, n-i):
-        print("*", end = "")
-    print()
-"""
-
-# 1356 사각형 출력하기 2
-
-"""
-n = int(input())
-
-for i in range(1, n+1):
-    if(i==1 or i==n):
-        for j in range(1, n+1):
-            print("*", end = "")
-        print()
-    else:
-        print("*", end = "")
-        for j in range(2, n):
-            print(" ", end = "")
-        print("*", end = "")
-        print()
-"""
-
-# 1357 삼각형 출력하기 4
-
-"""
-n = int(input())
-for i in range(1, n+1):
-    for j in range(0, i):
-        print("*", end = "")
-    print()
-for i in range(n+1, 1, -1):
-    for j in range(1, i-1):
-        print("*", end = "")
-    print()
-"""
-
-# 1358 삼각형 출력하기 5
-
-"""
-n = int(input())
-
-for i in range(int(n/2), -1, -1):
-    for j in range(0, i):
-        print(' ', end='')
-    for j in range(0, (n - i * 2)):
-        print('*', end='')
-    for j in range(0, i):
-        print(' ', end='')
-    print()
-"""
-
-# 1359 숫자 피라미드 1
-
-"""
-num = int(input())
-n = 1
-
-for i in range(1, num+1):
-    for j in range(1, i+1):
-        print(j, end = "")
-        n += 1
-    print()
-"""
-
-# 1360 숫자 피라미드 2
-
-"""
-num = int(input())
-n = num
-
-for i in range(num, 0, -1):
     for j in range(i, 0, -1):
-        print(i, end = "")
+        print(" ", end = " ")
+    for j in range(0, n):
+        print("*", end = " ")
     print()
 
-# 다른 버전
-num = int(input())
-n = num
+답:
+n = int(input())
 
-for i in range(num, 0, -1):
-    for j in range(i, 0, -1):
-        print(j, end = "")
-    n -= 1
+for i in range(n-1, -1, -1):
+    for j in range(0, i):
+        print(" ", end = " ")
+    for j in range(0, n):
+        print("*", end = " ")
     print()
+"""
+
+# 1368 평행사변형 출력하기 2
+
+"""
+
+a, b, c = input().split()
+a = int(a)
+b = int(b)
+
+if(c=='L'):
+    for i in range(0, a):
+        for j in range(i, 0, -1):
+            print(" ", end = " ")
+        for j in range(0, b):
+            print("*", end = " ")
+        print()
+
+else:
+    for i in range(a-1, -1, -1):
+        for j in range(0, i):
+            print(" ", end = " ")
+        for j in range(0, b):
+            print("*", end = " ")
+        print()
+"""
+
+# 1369 빗금 친 사각형 출력하기
+
+"""
+a, b = map(int, input().split())
+
+for i in range(1, a+1):
+    for j in range(1, a+1):
+        if(i==1 or i==a or j==1 or j==a):
+            print("*", end = " ")
+        elif(i+j-1) % b ==0:
+            print("*", end = " ")
+        else:
+            print(" ", end = " ")
+    print()
+"""
+
+# 1370 지그재그 출력하기
+
+"""
+h, r = map(int, input().split())
+​
+​
+def zigzag(h):
+    for i in range(0, h):
+        for j in range(0, i):
+            print(' ', end='')
+        print('*')
+    for i in range(h-2, -1, -1):
+        for j in range(0, i):
+            print(' ', end='')
+        print('*')
+​
+​
+for i in range(0, r):
+    zigzag(h)
 """
