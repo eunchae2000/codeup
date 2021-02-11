0851148eca8bd6d2ddb5083489790ec7b4eecc28
coco <tldjs3651@naver.com>
2021-02-11 22:00:29 +0900

    codeup 문제 1440-1345

--- test.py
+++ test.py
@@ -1,38 +1,63 @@
-# 1425 자리 배치
+# 1440 비교
+"""
+n = int(input())
+number = list(map(int, input().split()))
 
+for i in range(0, n):
+    num = number[i]
+    if(i==0):
+        num_list = number[1:]
+    elif(i==n-1):
+        num_list = number[:-1]
+    else:
+        num_list = number[0:i]+number[i+1:]
+    result = ''
+
+    for k in num_list:
+        if num < k:
+            result += ' < '
+        elif num == k:
+            result += ' = '
+        elif num > k:
+            result += ' > '
+    print(str(i+1)+":"+result)
+"""
+# 1441 버블 정렬
+"""
+n = int(input())
+num = list(map(int, input().split("\n")))
+num.sort()
+
+for i in num:
+    print(i, end = "\n")
+"""
+# 1442 선택 정렬
+"""
+n = int(input())
+num = list(map(int, input().split("\n")))
+num.sort()
+
+for i in num:
+    print(i, end = "\n")
+"""
+# 1443 삽입 정렬
+"""
+n = int(input())
+num = list(map(int, input().split("\n")))
+num.sort()
+
+for i in num:
+    print(i, end = "\n")
+"""
+# 1445 정렬된 두 배열 합치기
 """
 a, b = map(int, input().split())
-students = list(map(int, input().split()))
-
-students.sort()
-
-for i in range(0, int(a/b)+1):
-    for j in range(i*b, i*b+b):
-        if j>=len(students):
-            break
-        print(students[j], end = " ")
-    print()
-"""
-
-# 1430 기억력 테스트 2
-
-"""
-a = int(input())
-aa =list(map(int, input().split()))
-b = int(input())
+aa = list(map(int, input().split()))
 bb = list(map(int, input().split()))
-cc = []
 
-for i in bb:
-    for j in aa:
-        if(i == j):
-            cc[i] == 1
-        else:
-            cc[i] == 0
+result = aa + bb
+result.sort()
 
-for i in cc:
-    if(i == 1):
-        print("1", end = " ")
-    else:
-        print("0", end = " ")
+for i in result:
+    print(i, end = " ")
 """
