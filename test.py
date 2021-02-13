6adf2c65b27a231462783f78152d4d631d195369
coco <tldjs3651@naver.com>
2021-02-13 23:45:48 +0900

    codeup 문제 1451-1465

--- test.py
+++ test.py
@@ -1,63 +1,97 @@
-# 1440 비교
+
+# 1451 데이터 정렬 (small)
+
 """
 n = int(input())
-number = list(map(int, input().split()))
+num = list(map(int, input().split()))
+num.sort(reverse=False)
+
+for i in range(n):
+    print(num[i])
+"""
+
+# 1452 데이터 정렬 (large)
+
+"""
+n = int(input())
+num = list(map(int, input().split()))
+num.sort(reverse=True)
+
+for i in range(n):
+    print(num[i])
+"""
+
+# 1460 2차원 배열 순서대로 채우기 1-1
+
+"""
+n = int(input())
+sum1 = 0
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        sum1 += 1
+        print(sum1, end = " ")
+    print()
+"""
+
+# 1461 2차원 배열 순서대로 채우기 1-2
+
+"""
+n = int(input())
+sum1 = 0
 
 for i in range(0, n):
-    num = number[i]
-    if(i==0):
-        num_list = number[1:]
-    elif(i==n-1):
-        num_list = number[:-1]
-    else:
-        num_list = number[0:i]+number[i+1:]
-    result = ''
+    for j in range(1, n+1):
+        sum1 += 1
 
-    for k in num_list:
-        if num < k:
-            result += ' < '
-        elif num == k:
-            result += ' = '
-        elif num > k:
-            result += ' > '
-    print(str(i+1)+":"+result)
+for i in range(0, n):
+    for j in range(1, n+1):
+        print(sum1, end = " ")
+        sum1 -= 1
+    print()
 """
-# 1441 버블 정렬
+
+# 1462 2차원 배열 순서대로 채우기 1-3
+
 """
 n = int(input())
-num = list(map(int, input().split("\n")))
-num.sort()
 
-for i in num:
-    print(i, end = "\n")
+for i in range(1, n+1):
+    for j in range(0, n):
+        print(n*j+i, end = " ")
+    print()
 """
-# 1442 선택 정렬
+
+# 1463 2차원 배열 순서대로 채우기 1-4
+
 """
 n = int(input())
-num = list(map(int, input().split("\n")))
-num.sort()
 
-for i in num:
-    print(i, end = "\n")
+for i in range(0, n):
+    for j in range(1, n+1):
+        print(n*j-i, end = " ")
+    print()
 """
-# 1443 삽입 정렬
-"""
-n = int(input())
-num = list(map(int, input().split("\n")))
-num.sort()
 
-for i in num:
-    print(i, end = "\n")
-"""
-# 1445 정렬된 두 배열 합치기
+# 1464 2차원 배열 순서대로 채우기 1-5
+
 """
 a, b = map(int, input().split())
-aa = list(map(int, input().split()))
-bb = list(map(int, input().split()))
+result = 0
 
-result = aa + bb
-result.sort()
+for i in range(b, 0, -1):
+    for j in range(a, 0, -1):
+        result = i*j
+        print(result, end = " ")
+    print()
+"""
 
-for i in result:
-    print(i, end = " ")
+# 1465 2차원 배열 순서대로 채우기 1-6
+"""
+n, m = map(int, input().split())
+
+for i in range(n-1, -1, -1):
+    for j in range(1, m+1):
+        print(m*i+j, end = " ")
+    print()
 """
